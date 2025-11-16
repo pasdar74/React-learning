3f3bdc72fb768a53bb8c8dd90d98d06d4f2a9786
pasdar74 <mortazapasdar@gmail.com>
2025-11-16 21:27:35 +0330

    V 2.0.1

--- src/Pages/Users/UserList.jsx
+++ src/Pages/Users/UserList.jsx
@@ -1,8 +1,8 @@
 const Userlist = ()=>{
     return(
         <div className="border border-gray-200 rounded-2xl bg-white p-5">
-            <div className="flex justify-center">
-                <h1 className="text-4xl">
+            <div className="flex items-center justify-between">
+                <h1 className="text-xl/5 text-gray-900 ">
                     لیست کاربران
                 </h1>
             </div>
